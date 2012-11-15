335e57365c6913f40e470ee89317b95a66dc9b8f
Pawel Bedynski <bediej@bediej.(none)>
2012-11-15 23:22:12 +0000

    initial commit and folder structure

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: 'js/lib',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        app: '../app'
+    }
+});
+
+require(["jquery.min", "bootstrap.min", "d3.v2.min", "knockout"], 
+	
+	function($){ 
+       $(function(){
+          // carousel demo
+          $('#myCarousel').carousel();
+    	});
+	}
+);
+
