fc76541e09fe09d49e6c921d30f734d4e97946e7
Pawel Bedynski <p.bedynski@gmail.com>
2013-01-30 23:42:24 +0000

    git ignore + other folders

---
+++ public/data/translations.js
@@ -0,0 +1,20 @@
+var resources = {
+	LHouses : {
+	        "pl": "Domy",
+	        "en": "Houses"
+	    },
+	LApartments : {
+	        "pl": "Apartamenty",
+	        "en": "Apartments"
+	},
+
+	LStores: {
+	        "pl": "Magazyny",
+	        "en": "Stores"
+	},
+
+	LPenthouses : {
+	        "pl": "Penthouses",
+	        "en": "Penthouses"
+	}
+}
